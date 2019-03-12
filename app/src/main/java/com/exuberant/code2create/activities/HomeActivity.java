f3c641f62071418bf7732cd211f5352bdf7d4610
reallyinvincible <mailtosparshsri@gmail.com>
2019-03-13 02:47:54 +0530

    Navigation UI Completed

--- app/src/main/java/com/exuberant/code2create/activities/HomeActivity.java
+++ app/src/main/java/com/exuberant/code2create/activities/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.exuberant.code2create.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.widget.FrameLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.exuberant.code2create.FragmentSwitchInterface;
 import com.exuberant.code2create.R;
@@ -14,15 +17,25 @@ import com.exuberant.code2create.fragments.AgendaFragment;
 import com.exuberant.code2create.fragments.FoodCouponsFragment;
 import com.exuberant.code2create.fragments.PrizesFragment;
 import com.exuberant.code2create.fragments.SponsorsFragment;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.button.MaterialButton;
 
 public class HomeActivity extends AppCompatActivity {
 
     static FragmentSwitchInterface fragmentSwitchInterface;
+    private MaterialButton navigationButton, bookmarkButton, alertButton, closeButton;
+    private LinearLayout bottomSheetLayout;
+    private static final int BOOKMARK_SELECTED = 1;
+    private static final int ALERT_SELECTED = 2;
+    private int currentState;
+    BottomSheetBehavior bottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initializeViews();
 
         //HomeActivity.getFragmentSwitchInterface().switchToSponsors();
 
@@ -58,6 +71,62 @@ public class HomeActivity extends AppCompatActivity {
             }
         };
 
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int i) {
+                if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED){
+                    bottomSheetLayout.setBackground(getResources().getDrawable(R.drawable.rounded_corner_bottom_sheet_accent));
+                    navigationButton.setVisibility(View.VISIBLE);
+                    alertButton.setVisibility(View.VISIBLE);
+                    bookmarkButton.setVisibility(View.VISIBLE);
+                    closeButton.setVisibility(View.GONE);
+
+                } else {
+                    bottomSheetLayout.setBackground(getResources().getDrawable(R.drawable.rounded_corner_bottom_sheet_primary));
+                    navigationButton.setVisibility(View.GONE);
+                    alertButton.setVisibility(View.GONE);
+                    bookmarkButton.setVisibility(View.GONE);
+                    closeButton.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+
+        navigationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        alertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bookmarkButton.setIcon(getResources().getDrawable(R.drawable.ic_bookmark_outlined));
+                alertButton.setIcon(getResources().getDrawable(R.drawable.ic_alerts_filled));
+            }
+        });
+
+        bookmarkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bookmarkButton.setIcon(getResources().getDrawable(R.drawable.ic_bookmark_filled));
+                alertButton.setIcon(getResources().getDrawable(R.drawable.ic_alert_outlined));
+            }
+        });
+
     }
 
     void switchFragment(Fragment fragment){
@@ -72,4 +141,14 @@ public class HomeActivity extends AppCompatActivity {
     public static void setFragmentSwitchInterface(FragmentSwitchInterface fragmentSwitchInterface) {
         HomeActivity.fragmentSwitchInterface = fragmentSwitchInterface;
     }
+
+    void initializeViews(){
+        navigationButton = findViewById(R.id.btn_navigation);
+        alertButton = findViewById(R.id.btn_alerts);
+        bookmarkButton = findViewById(R.id.btn_bookmarks);
+        bottomSheetLayout = findViewById(R.id.bottom_sheet);
+        closeButton = findViewById(R.id.btn_close_bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
+    }
+
 }
