f0bf76cd31f0ce838ed362c8bab4f47efaa95a54
reallyinvincible <mailtosparshsri@gmail.com>
2019-03-12 23:08:01 +0530

    Fragment Interface Implemented

---
+++ app/src/main/java/com/exuberant/code2create/FragmentSwitchInterface.java
@@ -0,0 +1,11 @@
+package com.exuberant.code2create;
+
+public interface FragmentSwitchInterface {
+    void switchToAbout();
+    void switchToAgenda();
+    void switchToFaq();
+    void switchToCoupons();
+    void switchToPrizes();
+    void switchToSponsors();
+
+}
--- app/src/main/java/com/exuberant/code2create/activities/HomeActivity.java
+++ app/src/main/java/com/exuberant/code2create/activities/HomeActivity.java
@@ -1,16 +1,75 @@
 package com.exuberant.code2create.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import com.exuberant.code2create.FragmentSwitchInterface;
 import com.exuberant.code2create.R;
+import com.exuberant.code2create.fragments.AboutFragment;
+import com.exuberant.code2create.fragments.AgendaFragment;
+import com.exuberant.code2create.fragments.FoodCouponsFragment;
+import com.exuberant.code2create.fragments.PrizesFragment;
+import com.exuberant.code2create.fragments.SponsorsFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    static FragmentSwitchInterface fragmentSwitchInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //HomeActivity.getFragmentSwitchInterface().switchToSponsors();
+
+        fragmentSwitchInterface = new FragmentSwitchInterface() {
+            @Override
+            public void switchToAbout() {
+                switchFragment(new AboutFragment());
+            }
+
+            @Override
+            public void switchToAgenda() {
+                switchFragment(new AgendaFragment());
+            }
+
+            @Override
+            public void switchToFaq() {
+                switchFragment(new AgendaFragment());
+            }
+
+            @Override
+            public void switchToCoupons() {
+                switchFragment(new FoodCouponsFragment());
+            }
+
+            @Override
+            public void switchToPrizes() {
+                switchFragment(new PrizesFragment());
+            }
+
+            @Override
+            public void switchToSponsors() {
+                switchFragment(new SponsorsFragment());
+            }
+        };
+
+    }
+
+    void switchFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.fl_fragment_container, fragment).commit();
+    }
+
+    public static FragmentSwitchInterface getFragmentSwitchInterface() {
+        return fragmentSwitchInterface;
+    }
+
+    public static void setFragmentSwitchInterface(FragmentSwitchInterface fragmentSwitchInterface) {
+        HomeActivity.fragmentSwitchInterface = fragmentSwitchInterface;
     }
 }
