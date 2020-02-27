1a9783fe5bd55cdabec331716eaf9e95b1ddafa7
dev5151 <iamdev5151@gmail.com>
2020-02-27 19:50:12 +0530

    Uncommented Flag

--- app/src/main/java/com/exuberant/code2create/activities/HomeActivity.java
+++ app/src/main/java/com/exuberant/code2create/activities/HomeActivity.java
@@ -54,7 +54,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
         setContentView(R.layout.activity_home);
 
         initializeViews();
